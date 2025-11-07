a1c5913c015b6ce5bfffd0083c0fd4af29864724
Kian <105409698+knejadshamsi@users.noreply.github.com>
2025-11-07 13:47:00 -0500

    simulation area: components

---
+++ src/Services/ez/input/simulationAreaSection/types.ts
@@ -0,0 +1,18 @@
+import type { Coordinate, Zone } from '~ez/stores/types'
+import type { ZoneSessionData } from '~stores/session'
+
+export type OriginType = 'center' | 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right'
+
+export interface ScaleUpdate {
+  percentage?: number
+  origin?: OriginType
+}
+
+export interface ValidatedZone extends Zone {
+  coords: Coordinate[][]
+}
+
+export interface AreaColorPreset {
+  label: string
+  colors: string[]
+}
