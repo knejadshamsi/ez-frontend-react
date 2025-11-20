8a8893894371c6ad16cba904a586313cb6dd841b
Kian <105409698+knejadshamsi@users.noreply.github.com>
2025-11-20 18:47:23 -0500

    output stores architecture

---
+++ src/Services/ez/stores/output/defaults.ts
@@ -0,0 +1,45 @@
+/** Output Static Store Defaults for chart  and table **/
+
+import type {
+  EZPeopleResponseChartConfig,
+  EZTimeImpactChartConfig,
+  EZEmissionsBarChartConfig,
+  EZVehicleEmissionsChartConfig,
+  EZTripLegsTableConfig,
+} from './types';
+
+export const DEFAULT_PEOPLE_RESPONSE_CHART_CONFIG: EZPeopleResponseChartConfig = {
+  categoryIds: ['paidPenalty', 'rerouted', 'switchedToBus', 'switchedToSubway', 'switchedToWalking', 'switchedToBiking', 'cancelledTrip'],
+  categoryLabels: ['Paid Penalty', 'Rerouted', 'Changed to Bus', 'Changed to Subway', 'Changed to Walking', 'Changed to Biking', 'Trip Cancelled'],
+  categoryColors: ['#c9c9c9', '#ffd4a3', '#b8d4e8', '#a8c7dd', '#c8e6c9', '#b2dfb2', '#f5b7b1'],
+};
+
+export const DEFAULT_TIME_IMPACT_CHART_CONFIG: EZTimeImpactChartConfig = {
+  categoryIds: ['paidPenalty', 'rerouted', 'switchedToBus', 'switchedToSubway', 'switchedToWalking', 'switchedToBiking'],
+  categoryLabels: ['Paid Penalty', 'Rerouted', 'Changed to Bus', 'Changed to Subway', 'Changed to Walking', 'Changed to Biking'],
+  categoryColors: ['#c9c9c9', '#ffd4a3', '#b8d4e8', '#a8c7dd', '#c8e6c9', '#b2dfb2'],
+};
+
+export const DEFAULT_EMISSIONS_BAR_CHART_CONFIG: EZEmissionsBarChartConfig = {
+  pollutantIds: ['CO2', 'NOx', 'PM2.5', 'PM10'],
+  pollutantLabels: ['CO₂', 'NOx', 'PM2.5', 'PM10'],
+  baselineBarColor: '#d9cdc3',
+  postPolicyBarColor: '#b2dfb2',
+};
+
+export const DEFAULT_VEHICLE_EMISSIONS_CHART_CONFIG: EZVehicleEmissionsChartConfig = {
+  vehicleTypeIds: ['electric', 'standard', 'heavy'],
+  vehicleTypeLabels: ['Electric Vehicles', 'Standard Vehicles', 'Heavy Vehicles'],
+  vehicleTypeColors: ['#b2dfb2', '#b8d4e8', '#d9cdc3'],
+};
+
+export const DEFAULT_TRIP_LEGS_TABLE_CONFIG: EZTripLegsTableConfig = {
+  columns: [
+    { title: 'Person ID', dataIndex: 'personId', key: 'personId', width: 120 },
+    { title: 'From Activity', dataIndex: 'originActivity', key: 'originActivity', width: 150 },
+    { title: 'To Activity', dataIndex: 'destinationActivity', key: 'destinationActivity', width: 150 },
+    { title: 'CO₂ Δ (g)', dataIndex: 'co2DeltaGrams', key: 'co2DeltaGrams', width: 120, isSortable: true },
+    { title: 'Time Δ (min)', dataIndex: 'timeDeltaMinutes', key: 'timeDeltaMinutes', width: 120, isSortable: true },
+    { title: 'Impact', dataIndex: 'impact', key: 'impact', width: 150 },
+  ],
+};
---
+++ src/Services/ez/stores/output/stores.ts
@@ -0,0 +1,218 @@
+import { create } from 'zustand';
+import type {
+  EZOutputOverviewData,
+  EZEmissionsParagraph1Data,
+  EZEmissionsParagraph2Data,
+  EZEmissionsBarChartData,
+  EZEmissionsPieChartsData,
+  EZPeopleResponseParagraph1Data,
+  EZPeopleResponseParagraph2Data,
+  EZPeopleResponseBreakdownChartData,
+  EZPeopleResponseTimeImpactChartData,
+  EZTripLegRecord,
+  EZTripLegsPaginationInfo,
+  EZPeopleResponseChartConfig,
+  EZTimeImpactChartConfig,
+  EZEmissionsBarChartConfig,
+  EZVehicleEmissionsChartConfig,
+  EZTripLegsTableConfig,
+} from './types';
+import {
+  DEFAULT_PEOPLE_RESPONSE_CHART_CONFIG,
+  DEFAULT_TIME_IMPACT_CHART_CONFIG,
+  DEFAULT_EMISSIONS_BAR_CHART_CONFIG,
+  DEFAULT_VEHICLE_EMISSIONS_CHART_CONFIG,
+  DEFAULT_TRIP_LEGS_TABLE_CONFIG,
+} from './defaults';
+
+// === OVERVIEW STORE ===
+
+interface EZOutputOverviewStoreState {
+  overviewData: EZOutputOverviewData | null;
+  setOverviewData: (data: EZOutputOverviewData) => void;
+  resetOverviewStore: () => void;
+}
+
+export const useEZOutputOverviewStore = create<EZOutputOverviewStoreState>((set) => ({
+  overviewData: null,
+  setOverviewData: (overviewData) => set({ overviewData }),
+  resetOverviewStore: () => set({ overviewData: null }),
+}));
+
+// === EMISSIONS STORE ===
+
+interface EZOutputEmissionsStoreState {
+  emissionsParagraph1Data: EZEmissionsParagraph1Data | null;
+  emissionsParagraph2Data: EZEmissionsParagraph2Data | null;
+  emissionsBarChartData: EZEmissionsBarChartData | null;
+  emissionsPieChartsData: EZEmissionsPieChartsData | null;
+
+  setEmissionsParagraph1Data: (data: EZEmissionsParagraph1Data) => void;
+  setEmissionsParagraph2Data: (data: EZEmissionsParagraph2Data) => void;
+  setEmissionsBarChartData: (data: EZEmissionsBarChartData) => void;
+  setEmissionsPieChartsData: (data: EZEmissionsPieChartsData) => void;
+
+  resetEmissionsStore: () => void;
+}
+
+export const useEZOutputEmissionsStore = create<EZOutputEmissionsStoreState>((set) => ({
+  emissionsParagraph1Data: null,
+  emissionsParagraph2Data: null,
+  emissionsBarChartData: null,
+  emissionsPieChartsData: null,
+
+  setEmissionsParagraph1Data: (emissionsParagraph1Data) => set({ emissionsParagraph1Data }),
+  setEmissionsParagraph2Data: (emissionsParagraph2Data) => set({ emissionsParagraph2Data }),
+  setEmissionsBarChartData: (emissionsBarChartData) => set({ emissionsBarChartData }),
+  setEmissionsPieChartsData: (emissionsPieChartsData) => set({ emissionsPieChartsData }),
+
+  resetEmissionsStore: () => set({
+    emissionsParagraph1Data: null,
+    emissionsParagraph2Data: null,
+    emissionsBarChartData: null,
+    emissionsPieChartsData: null,
+  }),
+}));
+
+// === PEOPLE RESPONSE STORE ===
+
+interface EZOutputPeopleResponseStoreState {
+  peopleResponseParagraph1Data: EZPeopleResponseParagraph1Data | null;
+  peopleResponseParagraph2Data: EZPeopleResponseParagraph2Data | null;
+  peopleResponseBreakdownChartData: EZPeopleResponseBreakdownChartData | null;
+  peopleResponseTimeImpactChartData: EZPeopleResponseTimeImpactChartData | null;
+
+  setPeopleResponseParagraph1Data: (data: EZPeopleResponseParagraph1Data) => void;
+  setPeopleResponseParagraph2Data: (data: EZPeopleResponseParagraph2Data) => void;
+  setPeopleResponseBreakdownChartData: (data: EZPeopleResponseBreakdownChartData) => void;
+  setPeopleResponseTimeImpactChartData: (data: EZPeopleResponseTimeImpactChartData) => void;
+
+  resetPeopleResponseStore: () => void;
+}
+
+export const useEZOutputPeopleResponseStore = create<EZOutputPeopleResponseStoreState>((set) => ({
+  peopleResponseParagraph1Data: null,
+  peopleResponseParagraph2Data: null,
+  peopleResponseBreakdownChartData: null,
+  peopleResponseTimeImpactChartData: null,
+
+  setPeopleResponseParagraph1Data: (peopleResponseParagraph1Data) => set({ peopleResponseParagraph1Data }),
+  setPeopleResponseParagraph2Data: (peopleResponseParagraph2Data) => set({ peopleResponseParagraph2Data }),
+  setPeopleResponseBreakdownChartData: (peopleResponseBreakdownChartData) => set({ peopleResponseBreakdownChartData }),
+  setPeopleResponseTimeImpactChartData: (peopleResponseTimeImpactChartData) => set({ peopleResponseTimeImpactChartData }),
+
+  resetPeopleResponseStore: () => set({
+    peopleResponseParagraph1Data: null,
+    peopleResponseParagraph2Data: null,
+    peopleResponseBreakdownChartData: null,
+    peopleResponseTimeImpactChartData: null,
+  }),
+}));
+
+// === TRIP LEGS STORE ===
+
+interface TripLegsFirstPageData {
+  records: EZTripLegRecord[];
+  totalRecords: number;
+  pageSize: number;
+}
+
+interface EZOutputTripLegsStoreState {
+  tripLegRecords: EZTripLegRecord[];
+  tripLegsPagination: EZTripLegsPaginationInfo | null;
+  isTripLegsLoading: boolean;
+  tripLegsLoadError: string | null;
+
+  setTripLegRecords: (records: EZTripLegRecord[]) => void;
+  setTripLegsPagination: (pagination: EZTripLegsPaginationInfo) => void;
+  setTripLegsLoading: (isLoading: boolean) => void;
+  setTripLegsLoadError: (error: string | null) => void;
+  setTripLegsFirstPage: (data: TripLegsFirstPageData) => void;
+  setTripLegsPage: (page: number, records: EZTripLegRecord[]) => void;
+
+  resetTripLegsStore: () => void;
+}
+
+export const useEZOutputTripLegsStore = create<EZOutputTripLegsStoreState>((set) => ({
+  tripLegRecords: [],
+  tripLegsPagination: null,
+  isTripLegsLoading: false,
+  tripLegsLoadError: null,
+
+  setTripLegRecords: (tripLegRecords) => set({ tripLegRecords }),
+  setTripLegsPagination: (tripLegsPagination) => set({ tripLegsPagination }),
+  setTripLegsLoading: (isTripLegsLoading) => set({ isTripLegsLoading }),
+  setTripLegsLoadError: (tripLegsLoadError) => set({ tripLegsLoadError }),
+
+  setTripLegsFirstPage: ({ records, totalRecords, pageSize }) => set({
+    tripLegRecords: records,
+    tripLegsPagination: {
+      currentPage: 1,
+      pageSize,
+      totalRecords,
+      totalPages: Math.ceil(totalRecords / pageSize),
+    },
+  }),
+
+  setTripLegsPage: (page, records) => set((state) => ({
+    tripLegRecords: records,
+    tripLegsPagination: state.tripLegsPagination
+      ? { ...state.tripLegsPagination, currentPage: page }
+      : null,
+  })),
+
+  resetTripLegsStore: () => set({
+    tripLegRecords: [],
+    tripLegsPagination: null,
+    isTripLegsLoading: false,
+    tripLegsLoadError: null,
+  }),
+}));
+
+// === MAP READY FLAGS STORE ===
+
+interface EZOutputMapReadyStoreState {
+  isEmissionsMapDataReady: boolean;
+  isPeopleResponseMapDataReady: boolean;
+  isTripLegsMapDataReady: boolean;
+
+  setEmissionsMapDataReady: (isReady: boolean) => void;
+  setPeopleResponseMapDataReady: (isReady: boolean) => void;
+  setTripLegsMapDataReady: (isReady: boolean) => void;
+
+  resetMapReadyStore: () => void;
+}
+
+export const useEZOutputMapReadyStore = create<EZOutputMapReadyStoreState>((set) => ({
+  isEmissionsMapDataReady: false,
+  isPeopleResponseMapDataReady: false,
+  isTripLegsMapDataReady: false,
+
+  setEmissionsMapDataReady: (isEmissionsMapDataReady) => set({ isEmissionsMapDataReady }),
+  setPeopleResponseMapDataReady: (isPeopleResponseMapDataReady) => set({ isPeopleResponseMapDataReady }),
+  setTripLegsMapDataReady: (isTripLegsMapDataReady) => set({ isTripLegsMapDataReady }),
+
+  resetMapReadyStore: () => set({
+    isEmissionsMapDataReady: false,
+    isPeopleResponseMapDataReady: false,
+    isTripLegsMapDataReady: false,
+  }),
+}));
+
+// === CHART CONFIGURATION STORE ===
+
+interface EZOutputChartConfigStoreState {
+  peopleResponseChartConfig: EZPeopleResponseChartConfig;
+  timeImpactChartConfig: EZTimeImpactChartConfig;
+  emissionsBarChartConfig: EZEmissionsBarChartConfig;
+  vehicleEmissionsChartConfig: EZVehicleEmissionsChartConfig;
+  tripLegsTableConfig: EZTripLegsTableConfig;
+}
+
+export const useEZOutputChartConfigStore = create<EZOutputChartConfigStoreState>(() => ({
+  peopleResponseChartConfig: DEFAULT_PEOPLE_RESPONSE_CHART_CONFIG,
+  timeImpactChartConfig: DEFAULT_TIME_IMPACT_CHART_CONFIG,
+  emissionsBarChartConfig: DEFAULT_EMISSIONS_BAR_CHART_CONFIG,
+  vehicleEmissionsChartConfig: DEFAULT_VEHICLE_EMISSIONS_CHART_CONFIG,
+  tripLegsTableConfig: DEFAULT_TRIP_LEGS_TABLE_CONFIG,
+}));
